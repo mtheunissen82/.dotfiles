cf5ee9bc1017b7a9531737a0a951f173da1178bd
Marc Theunissen <mtheunissen82@gmail.com>
2020-03-26 09:20:41 +0100

    Add flexible io tester (seen in cloudflare blog)

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install apt packages
-sudo apt-get install -y vim vim-gnome neovim curl wget git silversearcher-ag tmux nfs-common samba-common-bin smbclient cifs-utils xclip fonts-powerline htop tree pass bridge-utils apache2-utils siege shellcheck
+sudo apt-get install -y vim vim-gnome neovim curl wget git silversearcher-ag tmux nfs-common samba-common-bin smbclient cifs-utils xclip fonts-powerline htop tree pass bridge-utils apache2-utils siege shellcheck fio
 
 # Install nvm (node version manager)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
