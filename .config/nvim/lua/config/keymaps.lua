7a084e8d07324274e823a1042fa02121d50bb2a4
Marc Theunissen <emtyr@protonmail.com>
2025-11-14 14:51:03 +0100

    Change telescope shortcuts

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -49,8 +49,9 @@ map("v", "<C-/>", "gc", { remap = true })
 -- Plugin: telescope.nvim
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope find files" })
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Telescope live grep" })
-map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Telescope find buffers" })
+map("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Telescope find buffers" })
 map("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Telescope find keymaps" })
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Telescope find previously opened files" })
 
 -- Plugin: neo-tree.nvim
 map("n", "<leader>e", "<cmd>Neotree toggle=true<cr>", { desc = "neo-tree toggle file explorer" })
