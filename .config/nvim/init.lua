a6cb1b7f1f60b2fede599ca66b75f68e0959f5df
Marc Theunissen <emtyr@protonmail.com>
2025-11-14 17:49:07 +0100

    Fix tree-sitter setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,4 +5,29 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 
+-- tree-sitter must explicitly be configured in init.lua otherwise it won't work
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "bash",
+    "css",
+    "html",
+    "java",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "python",
+    "toml",
+    "tsx",
+    "typescript",
+    "yaml",
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+})
+
 vim.cmd("colorscheme github_dark")
