660825ccffeb2ee75b7d3ec2f26a636fcb911989
Marc Theunissen <emtyr@protonmail.com>
2025-11-20 17:58:36 +0100

    nvim(ide): accept autocomplete on enter key

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,7 +3,9 @@ return {
     "saghen/blink.cmp",
     -- use a release tag to download pre-built binaries
     version = "1.*",
-    opts = {},
+    opts = {
+      keymap = { preset = "enter" },
+    },
   },
   {
     "neovim/nvim-lspconfig",
