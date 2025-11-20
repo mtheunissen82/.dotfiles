6e3999fdfa1caa45c423680760fac081511a0142
Marc Theunissen <emtyr@protonmail.com>
2025-11-20 17:56:48 +0100

    nvim(neo-tree): group empty dirs

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -11,14 +11,18 @@ return {
     enable_git_status = true,
     close_if_last_window = true,
     filesystem = {
+      group_empty_dirs = true,
       follow_current_file = {
         enabled = true,
+        leave_dirs_open = false,
       },
       use_libuv_file_watcher = true,
     },
     buffers = {
+      group_empty_dirs = true,
       follow_current_file = {
         enabled = true,
+        leave_dirs_open = false,
       },
     },
   },
