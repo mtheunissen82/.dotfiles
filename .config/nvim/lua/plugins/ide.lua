f822059a20e4bfc6ea6097e6530fb8f1e378c6ba
Marc Theunissen <emtyr@protonmail.com>
2025-11-27 11:29:05 +0100

    nvim(ide): add some blink.cmp configuration options

--- .config/nvim/lua/plugins/ide.lua
+++ .config/nvim/lua/plugins/ide.lua
@@ -1,10 +1,22 @@
 return {
   {
     "saghen/blink.cmp",
-    -- use a release tag to download pre-built binaries
+    dependencies = { "rafamadriz/friendly-snippets" },
     version = "1.*",
     opts = {
       keymap = { preset = "enter" },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
   },
   {
