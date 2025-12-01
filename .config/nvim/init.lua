f7f1764f1c8247c90a808e66dd8e61058c08a6cb
Marc Theunissen <emtyr@protonmail.com>
2025-12-01 09:23:16 +0100

    nvim(config): setup lsp diagnostics and various options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,6 @@
--- Disable startup splash screen
-vim.cmd("set shortmess+=I")
-
 require("config.options")
 require("config.keymaps")
+require("config.lsp")
 require("config.lazy")
 
 vim.cmd("colorscheme catppuccin")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,12 +9,19 @@ g.maplocalleader = "\\"
 o.clipboard = "unnamedplus"
 o.timeoutlen = 400
 o.hidden = false
+o.shortmess:append("I") -- Stop showing the intro splash screen
 
 -- UI
 o.cursorline = true
-o.wrap = false
-o.signcolumn = "yes"
 o.laststatus = 3
+o.scrolloff = 999 -- Keep cursor centered vertically
+o.signcolumn = "yes"
+o.splitbelow = true -- When opening a new horizontal split, put it below the current window
+o.wrap = false -- Disable line wrapping
+o.termguicolors = true
+o.virtualedit = "block" -- Allow cursor to move freely in visual block mode
+o.inccommand = "split" -- Show effects of substitutions incrementally in a split
+o.winborder = "rounded" -- Apply rounded borders for floating windows
 
 -- Line numbers
 o.number = true
