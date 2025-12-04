27a9c932a25f2eae4393e9982b13d8d006da310c
Marc Theunissen <emtyr@protonmail.com>
2025-12-04 09:22:00 +0100

    nvim(config): change keymap for deleting current buffer to be better accessible from the home-row

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -41,7 +41,7 @@ map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", {
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- Delete current buffer
-map("n", "<C-z>", "<cmd>bdelete!<cr>", { desc = "Delete current buffer" })
+map("n", "<leader>z", "<cmd>bdelete!<cr>", { desc = "Delete current buffer" })
 
 -- Git
 map("n", "<leader>grf", "<cmd>!git restore %<cr><esc>", { desc = "Git restore current file" })
