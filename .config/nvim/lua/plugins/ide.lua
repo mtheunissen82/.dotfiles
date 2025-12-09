43cfea80badf014870e6f84713ffd6ecc0a87513
Marc Theunissen <emtyr@protonmail.com>
2025-12-09 13:18:59 +0100

    nvim(ide): add opencode plugin

--- .config/nvim/lua/plugins/ide.lua
+++ .config/nvim/lua/plugins/ide.lua
@@ -205,4 +205,15 @@ return {
     },
   },
   { "akinsho/toggleterm.nvim", version = "*", opts = {} },
+  {
+    "NickvanDyke/opencode.nvim",
+    dependencies = {
+      ---@module "snacks"
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {}
+    end,
+  },
 }
